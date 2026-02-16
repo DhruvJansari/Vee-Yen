4469a77a158fc6114d8961a0fc74f8b10f2ab232
DhruvJansari <dhruvjansari281@gmail.com>
2026-02-16 12:17:59 +0530

    syntex error fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,26 @@
 // src/app/layout.tsx
 
+import type { Metadata } from "next";
 import { ReactNode } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-// import Footer from "./components/Footer";
 import "./globals.css";
-
 
 export const metadata: Metadata = {
   title: "Vee Yen Traders",
   description: "Vee Yen Traders Official Website",
-  icons: "/logo.png"
+  icons: {
+    icon: "/logo.png", // must be inside object
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning={true}>
-      <head />
+    <html lang="en" suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow">{children}</main>
-        {/* <Footer /> */}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
